fb42c235e6cd182ae5b2b995ada63fccc2153277
EricLou-mac <ericdongqilu@gmail.com>
2019-07-28 21:33:37 +1000

    f1

--- R/f10.R
+++ R/f10.R
@@ -11,8 +11,43 @@
 
 SET.TOKEN <- function(...) {
   Dots <- list(...)
-  print(names(Dots))
-
   chosenTOKEN  <<- Dots$SetToken
 
 }
+
+
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param ... Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#'
+#'
+#'
+
+
+SET.QUERY <- function(...){
+  Dots <- list(...)
+  #print(names(Dots))
+
+  #Query.6()
+  #Vars
+  chosensinceTransactionID <<- Dots$SetchosensinceTransactionID %>% ifelse(is.null(.),NA,.)
+
+  #Query.7()
+  #Vars
+  chosenCount              <<- Dots$SetchosenCount %>% ifelse(is.null(.),NA,.)
+  chosenPrice              <<- Dots$SetchosenPrice %>% ifelse(is.null(.),NA,.)
+  chosenGranularity        <<- Dots$SetchosenGranularity %>% ifelse(is.null(.),NA,.)
+  chosenFrom               <<- Dots$SetchosenFrom %>% ifelse(is.null(.),NA,.)#"2019-04-21"
+  chosenTo                 <<- Dots$SetchosenTo %>% ifelse(is.null(.),NA,.)#"2019-04-22"
+
+  #Query.30()
+  #Vars
+  chosenID                 <<- Dots$SetchosenID %>% ifelse(is.null(.),NA,.)
+  #  print(EpQuery)
+
+}
+
